398961f3337291fdc317c10aefb2c6c76f345711
Daniel Wang <u7918232@anu.edu.au>
2024-09-12 12:10:37 +1000

    training setup

--- scene/__init__.py
+++ scene/__init__.py
@@ -40,9 +40,9 @@ class Scene:
 
         self.train_cameras = {}
         self.test_cameras = {}
-
         if os.path.exists(os.path.join(args.source_path, f"/scenes/{scene}/sparse")):
-            scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.eval)
+            #scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.eval)
+            scene_info = sceneLoadTypeCallbacks["Colmap"](os.path.join(args.source_path, f"/scenes/{scene}/sparse"), args.images, args.eval)
         elif os.path.exists(os.path.join(args.source_path, "transforms_train.json")):
             print("Found transforms_train.json file, assuming Blender data set!")
             scene_info = sceneLoadTypeCallbacks["Blender"](args.source_path, args.white_background, args.eval)
