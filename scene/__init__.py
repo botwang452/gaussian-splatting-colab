8bee8832b6fe1d3b5e94e1466bd38ae541c31114
Daniel Wang <u7918232@anu.edu.au>
2024-09-13 09:50:24 +1000

    training setup

--- scene/__init__.py
+++ scene/__init__.py
@@ -18,7 +18,7 @@ from scene.gaussian_model import GaussianModel
 from arguments import ModelParams
 from utils.camera_utils import cameraList_from_camInfos, camera_to_JSON
 
-scene = 'train'
+scene = 'treehill'
 class Scene:
 
     gaussians : GaussianModel
